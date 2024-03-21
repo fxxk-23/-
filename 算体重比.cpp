27c0220239ad0c3939c03460fb5e5176d5a2583e
fxxk-23 <151443378+fxxk-23@users.noreply.github.com>
2024-03-21 12:49:50 +0800

    Add files via upload

---
+++ 算体重比.cpp
@@ -0,0 +1,9 @@
+﻿#include<stdio.h>
+int main(void) {
+	printf("请输入体重，身高");
+		double T, S, A;
+	scanf_s("%lf,%lf", &T, &S);
+	A = T / (S * S);
+	printf("体重比为：%lf", A);
+	return 0;
+}
---
+++ 算水费.cpp
@@ -0,0 +1,16 @@
+﻿#include<stdio.h>
+int main(void) {
+	int x, y;
+	printf("请输入用水量");
+	scanf_s("%d", &x);
+	if (x <= 15)
+	{
+		y = 4*x / 3;
+
+	}
+	else {
+		y = 2.5*x - 10.5;
+	}
+	printf("水费：%d", y);
+	return 0;
+}
